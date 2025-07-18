5ec9e9a05a6f9a2d73b7e53a92484647ade7dec0
pdesai7245 <pushkardesai123@gmail.com>
2025-07-18 15:59:46 +0530

    Added animated background blob to App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
 import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="bg-gradient-animate text-white min-h-screen font-inter">
+    <div className="relative overflow-hidden bg-gradient-animate text-white min-h-screen font-inter">
+      {/* Floating Animated Blob */}
+      <div className="animated-blob"></div>
+
       {/* Navbar */}
       <Navbar />
 
       {/* Hero Section */}
       <motion.section
         id="home"
-        className="pt-24 flex flex-col items-center justify-center h-[80vh] text-center"
+        className="pt-24 flex flex-col items-center justify-center h-[80vh] text-center z-10 relative"
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
@@ -44,7 +46,6 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
-
     </div>
   );
 }
